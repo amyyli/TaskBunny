8b6322dfaf570b43a0cfae8dda09f74218a75b3a
Cody Scholberg <cscholberg@gmail.com>
2015-07-02 18:33:52 -0400

    added profile view

---
+++ client/app/components/profile/profileController.js
@@ -0,0 +1,11 @@
+(function(){
+
+//load module
+angular.module('trApp')
+  .controller('profileController', ['$scope', '$location', profileController]);
+
+  function profileController() {
+
+  }
+
+})();
