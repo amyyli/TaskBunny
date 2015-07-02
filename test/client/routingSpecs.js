48d857d1bc2f224614e004f837d6ca3f5f21b333
Tyler Travers <ttravers17@gmail.com>
2015-07-02 19:02:32 -0400

    added testing for routes

--- test/client/routingSpecs.js
+++ test/client/routingSpecs.js
@@ -7,30 +7,39 @@ describe('Routing', function () {
   }));
 
   it('Should have / route, template, and controller', function () {
-    expect($route.routes['/']).to.be.ok;;
+    expect($route.routes['/']).to.be.ok;
+    expect($route.routes['/'].controller).to.equal('LandingPageController');
+    expect($route.routes['/'].templateUrl).to.equal('app/components/landing/landing.html');
   });
 
-  // it('Should have /signup route, template, and controller', function () {
-  //   expect($route.routes['/signup']).to.be.ok();
-  //   expect($route.routes['/signup'].controller).to.be('AuthController');
-  //   expect($route.routes['/signup'].templateUrl).to.be('app/auth/signup.html');
-  // });
+  it('Should have /sign-in route, template, and controller', function () {
+    expect($route.routes['/sign-in']).to.be.ok;
+    expect($route.routes['/sign-in'].controller).to.equal('SignInPageController');
+    expect($route.routes['/sign-in'].templateUrl).to.equal('app/components/sign-in/sign-in.html');
+  });
 
-  // it('Should have /signin route, template, and controller', function () {
-  //   expect($route.routes['/signin']).to.be.ok();
-  //   expect($route.routes['/signin'].controller).to.be('AuthController');
-  //   expect($route.routes['/signin'].templateUrl).to.be('app/auth/signin.html');
-  // });
+  it('Should have /create-task route, template, and controller', function () {
+    expect($route.routes['/create-task']).to.be.ok;
+    expect($route.routes['/create-task'].controller).to.equal('TaskFormController');
+    expect($route.routes['/create-task'].templateUrl).to.equal('app/components/taskNew/task-form.html');
+  });
 
-  // it('Should have /links route, template, and controller', function () {
-  //   expect($route.routes['/links']).to.be.ok();
-  //   expect($route.routes['/links'].controller).to.be('LinksController');
-  //   expect($route.routes['/links'].templateUrl).to.be('app/links/links.html');
-  // });
+  it('Should have /tasks route, template, and controller', function () {
+    expect($route.routes['/tasks']).to.be.ok;
+    expect($route.routes['/tasks'].controller).to.equal('TasksController');
+    expect($route.routes['/tasks'].templateUrl).to.equal('app/components/myTasks/tasks.html');
+  });
 
-  // it('Should have /shorten route, template, and controller', function () {
-  //   expect($route.routes['/shorten']).to.be.ok();
-  //   expect($route.routes['/shorten'].controller).to.be('ShortenController');
-  //   expect($route.routes['/shorten'].templateUrl).to.be('app/shorten/shorten.html');
-  // });
+  it('Should have /search route, template, and controller', function () {
+    expect($route.routes['/search']).to.be.ok;
+    expect($route.routes['/search'].controller).to.equal('TaskSearchController');
+    expect($route.routes['/search'].templateUrl).to.equal('app/components/taskSearch/taskSearch.html');
+  });
+
+  it('Should have /settings route, template, and controller', function () {
+    expect($route.routes['/settings']).to.be.ok;
+    expect($route.routes['/settings'].controller).to.equal('SettingsController');
+    expect($route.routes['/settings'].templateUrl).to.equal('app/components/userSettings/template.html');
+  });
+
 });
