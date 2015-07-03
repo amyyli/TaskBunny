9b476a6d18ce100829d3523d10de7dc3d8378091
Cody Scholberg <cscholberg@gmail.com>
2015-07-02 20:09:45 -0400

    added review button

--- client/app/components/profile/profileController.js
+++ client/app/components/profile/profileController.js
@@ -5,6 +5,11 @@ angular.module('trApp')
   .controller('profileController', ['$scope', '$location', profileController]);
 
   function profileController() {
+    $scope.name = '';
+    $scope.age = 0;
+    $scope.sex = 'yesPlease';
+    $scope.carrots = ;
+    $scope.review = 'haaaaaaaaaah bajiba jiba jaaaaaah';
 
   }
 
