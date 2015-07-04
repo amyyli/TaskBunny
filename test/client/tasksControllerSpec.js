97b4e55d86f1674668cbff3b240f970223e723c6
Tyler Travers <ttravers17@gmail.com>
2015-07-03 20:25:46 -0400

    have tests for tasks

--- test/client/tasksControllerSpec.js
+++ test/client/tasksControllerSpec.js
@@ -1,14 +1,20 @@
 "use strict";
 
 describe('TasksController', function () {
-  var $scope, $rootScope, createController, TaskService, $httpBackend;
+  var 
+  $scope, $rootScope, $location, 
+  createController, TaskService, 
+  $httpBackend, q, sandbox;
 
   beforeEach(module('trApp'));
 
   beforeEach(inject(function($injector) {
     // mock out our dependencies
+    sandbox = sinon.sandbox.create();
+    q = $injector.get('$q');
     $rootScope = $injector.get('$rootScope');
     $httpBackend = $injector.get('$httpBackend');
+    $location = $injector.get('$location');
     TaskService = $injector.get('TaskService');
     $scope = $rootScope.$new();
 
@@ -17,63 +23,37 @@ describe('TasksController', function () {
     createController = function () {
       return $controller('TasksController', {
         $scope: $scope,
+        $location: $location, 
         TaskService: TaskService
       });
     };
+
+    $httpBackend.whenGET("/api/mytasks").respond({ hello: 'World' });
+    createController();
   }));
 
-  afterEach(function() {
-    $httpBackend.verifyNoOutstandingExpectation();
-    $httpBackend.verifyNoOutstandingRequest();
+  afterEach(function () {
+    sandbox.restore();
+  //   $httpBackend.verifyNoOutstandingExpectation();
+  //   $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('Should make a get request to /api/mytasks', function () {
+    $httpBackend.expectGET('/api/mytasks').respond(200, '');
   });
 
   it('Should have a viewTask() method on the scope', function () {
-    createController();
     expect($scope.viewTask).to.be.a('function');
   });
 
+  it('Should have tasks', function (done) {
+    TaskService.retrieveUserTasks();
+    console.log($scope);
+
+    done();
+    // expect(TaskService.retrieveUserTasks())
+    // sandbox.stub(TaskService, 'retrieveUserTasks');
+
+  });
+  
 });
-
-// "use strict";
-
-// describe('LinksController', function () {
-//   var $scope, $rootScope, createController, Links, $httpBackend;
-
-//   // using angular mocks, we can inject the injector
-//   // to retrieve our dependencies
-//   beforeEach(module('shortly'));
-//   beforeEach(inject(function($injector) {
-
-//     // mock out our dependencies
-//     $rootScope = $injector.get('$rootScope');
-//     $httpBackend = $injector.get('$httpBackend');
-//     Links = $injector.get('Links');
-//     $scope = $rootScope.$new();
-
-//     var $controller = $injector.get('$controller');
-
-//     createController = function () {
-//       return $controller('LinksController', {
-//         $scope: $scope,
-//         Links: Links
-//       });
-//     };
-//   }));
-
-//   it('should have a data property on the $scope', function() {
-//     createController();
-//     expect($scope.data).to.be.an('object');
-//   });
-
-//   it('should have a getLinks method on the $scope', function () {
-//     createController();
-//     expect($scope.getLinks).to.be.a('function');
-//   });
-//   it('should call getLinks() when controller is loaded', function () {
-//     var mockLinks = [{},{},{}];
-//     $httpBackend.expectGET("/api/links").respond(mockLinks);
-//     createController();
-//     $httpBackend.flush();
-//     expect($scope.data.links).to.eql(mockLinks);
-//   });
-// });
